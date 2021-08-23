0550921d5dd24cd4db2761d7f414a4f04235a9f6
shogo928 <s.shogo.dev@gmail.com>
2021-08-23 22:29:50 +0900

    first commit

---
+++ Oboegaki-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Oboegaki-iOS
+//
+//  Created by sakumashogo on 2021/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Oboegaki-iOS/Oboegaki_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oboegaki_iOSApp.swift
+//  Oboegaki-iOS
+//
+//  Created by sakumashogo on 2021/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Oboegaki_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
