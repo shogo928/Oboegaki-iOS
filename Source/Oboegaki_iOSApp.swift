ec9213a722c0b610dddb8dda6e2d30ee24fe9a07
shogo928 <s.shogo.dev@gmail.com>
2021-08-24 22:19:58 +0900

    Fix Oboegaki_iOSApp.swift

--- Source/Oboegaki_iOSApp.swift
+++ Source/Oboegaki_iOSApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleSignIn
 
 @main
 struct Oboegaki_iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let viewModel = StartupViewModel()
+            StartupView(viewModel: viewModel)
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
     }
 }
