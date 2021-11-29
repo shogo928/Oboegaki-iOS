9b5a185b4b9e387418f009ba354d605e3374efd1
shogo928 <s.shogo.dev@gmail.com>
2021-11-29 11:05:42 +0900

    Fix Oboegaki_iOSApp

--- Source/Oboegaki_iOSApp.swift
+++ Source/Oboegaki_iOSApp.swift
@@ -13,10 +13,15 @@ import GoogleSignIn
 struct Oboegaki_iOSApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
+        
         WindowGroup {
+            
             let viewModel = StartupViewModel()
+            
             StartupView(viewModel: viewModel)
+            
         }
+        
     }
 }
 
@@ -24,12 +29,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         
+        // ログアウト
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            
+            try firebaseAuth.signOut()
+            
+        } catch let signOutError as NSError {
+            
+            print("Error signing out: %@", signOutError)
+            
+        }
+        
         return true
     }
     
     @available(iOS 9.0, *)
     func application(_ application: UIApplication, open url: URL,
                      options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+        
       return GIDSignIn.sharedInstance.handle(url)
+        
     }
 }
