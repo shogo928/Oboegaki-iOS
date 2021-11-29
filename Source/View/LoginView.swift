17b34b8e6ecbfcdb87a5d9882161c23c06b6cb97
shogo928 <s.shogo.dev@gmail.com>
2021-11-29 11:07:56 +0900

    Fix LoginView

--- Source/View/LoginView.swift
+++ Source/View/LoginView.swift
@@ -19,99 +19,171 @@ struct LoginView<T>: View where T: LoginViewModelObject {
     
     var body: some View {
         ZStack {
+            
+            // Background Gradient Effect
+
             LinearGradient(gradient: Gradient(colors: [Color("Primary"), .white]),
                            startPoint: .top,
                            endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack {
-                Spacer()
-                
-                TitllText
-                
-                Spacer().frame(height:20)
-                
-                signInStatusText
-                
-                Spacer().frame(height:20)
-
-                Group {
-                    Group {
-                        VStack {
-                            HStack {
+            
+            
+            GeometryReader { geometry in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 0) {
+                        
+                        // Login View
+                        
+                        VStack(alignment: .center, spacing: 0) {
+                            
+                            TitllText.padding(.vertical, 20)
+                            
+                            
+                            
+                            // Email
+                            
+                            VStack(alignment: .leading, spacing: 10) {
+                                
                                 emailHintText
-                                Spacer()
-                            }
-                            emailTextField
-                        }.padding(.horizontal, 10)
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.white)
-                            .cornerRadius(24)
-                    }
-                    
-                    Spacer().frame(height:20)
-                    
-                    Group {
-                        VStack {
-                            HStack {
+                                
+                                emailTextField.frame(width: geometry.size.width - 20, height: 25)
+                                
+                                        Rectangle()
+                                            .frame(height: 0.5)
+                                            .foregroundColor(Color(.white))
+                                            .cornerRadius(24)
+                                
+                            }.padding(.horizontal, 10)
+                            .padding(.bottom, 10)
+                            
+                            
+                            
+                            // Password
+                            
+                            VStack(alignment: .leading, spacing: 10) {
+                                
                                 passwordHintText
-                                Spacer()
-                            }
-                            if !viewModel.binding.isEntryPasswordShowFlag {
-                                passwordHideTextField
-                            } else {
-                                passwordOpenTextField
-                            }
-                        }.padding(.horizontal, 10)
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.white)
-                            .cornerRadius(24)
-                        HStack {
-                            Spacer()
-                            passwordShowActionText
-                        }.padding(.horizontal, 10)
-                    }.onChange(of: viewModel.binding.isEntryPasswordTextField) { _ in
-                        viewModel.input.toPasswordTextFieldEntered.send(viewModel.binding.isEntryPasswordTextField)
-                    }
-                    
-                    Spacer().frame(height:20)
-                    
-                    emailLoginButton
-                    
-                    Spacer().frame(height:20)
-                    
-                    termsOfServiceText
-                    
-                    Spacer().frame(height:20)
+                                
+                                if !viewModel.binding.isEntryPasswordShowFlag {
+                                    
+                                    passwordHideTextField.frame(width: geometry.size.width - 20, height: 25)
+                                    
+                                } else {
+                                    
+                                    passwordOpenTextField.frame(width: geometry.size.width - 20, height: 25)
+                                    
+                                }
+                                
+                                Rectangle()
+                                    .frame(height: 0.5)
+                                    .foregroundColor(Color(.white))
+                                    .cornerRadius(24)
+                                
+                                HStack {
 
-                    privacyPolicyText
-                }.padding(.horizontal, 20)
+                                    Spacer()
+                                    
+                                    passwordShowActionText
+                                    
+                                }.padding(.trailing, 10)
+                                
+                            }.padding(.horizontal, 10)
+                            
+                            .onChange(of: viewModel.binding.isEntryPasswordTextField) { _ in
+                                
+                                viewModel.input.toPasswordTextFieldEntered.send(viewModel.binding.isEntryPasswordTextField)
+                            
+                            }
+                            
+                            
+                            
+                            // Save Button
+                            
+                            VStack(alignment: .center, spacing: 0) {
+                                
+                                signInErrorText
+                                    .frame(height:30)
+                                    .padding(.vertical, 10)
+
+                                emailLoginButton
+
+                                HStack(alignment: .center, spacing: 15) {
+                                    
+                                    termsOfServiceButton
+
+                                    privacyPolicyButton
+
+                                }.padding(.vertical, 30)
+                                
+                            }.padding(.horizontal, 10)
+
+                            
+                            
+                            // Auth Login Button
+                            
+                            VStack(alignment: .center, spacing: 15) {
+                                
+                                accountLoginText.padding(.bottom, 10)
+                                
+                                signInWithAppleButton
+
+                                signInWithGoogleButton
+                                
+                                signInWithAnonymouslyButton
+
+                            }
+                            .padding(.horizontal, 10)
+                            
+                        }.frame(width: geometry.size.width, height: geometry.size.height)
+                        
+                        
+                        
+                        // Check Email View
+                        
+                        VStack(spacing: 0) {
+                            
+                            Spacer()
+                            
+                            Text("メールを送信しました\n送付先：\(viewModel.binding.isEntryEmailTextField)\nメールボックスを確認してください")
+                                .font(.system(size: 16, weight: .bold, design: .default))
+                                .foregroundColor(.white)
+                            
+                            Spacer()
+                            
+                            Text("\(viewModel.output.isSignInResultMessege)")
+                                .font(.system(size: 16, weight: .bold, design: .default))
+                                .foregroundColor(.white)
+                            
+                            Spacer()
+                            
+                            Button(
+                                action: {
+                                    viewModel.input.toReWrithButtonTapped.send()
+                                }, label: {
+                                    Text("入力し直す")
+                                        .frame(width: 200, height: 40, alignment: .center)
+                                        .font(.system(size: 16, weight: .regular, design: .default))
+                                        .background(Color("Primary"))
+                                        .foregroundColor(.white)
+                                        .cornerRadius(9)
+                                }
+                            ).frame(width: 200, height: 40, alignment: .center)
+                            
+                            Spacer()
+                            
+                        }.frame(width: geometry.size.width, height: geometry.size.height)
+                    }
+                }.content.offset(x: CGFloat(viewModel.binding.isScrollOffsetFlag ? -geometry.size.width : 0.0))
+                .animation(.default)
+                .frame(width: geometry.size.width * 2)
                 
-                Spacer()
-                
-                Group {
-                    accountLoginText
-                    
-                    Spacer().frame(height:26)
-                    
-                    signInWithGoogleButton
-                    
-                    Spacer().frame(height:26)
-                    
-                    signInWithAppleButton
-                    
-                    Spacer().frame(height:100)
-                }
-            }.onChange(of: viewModel.binding.isSignInStatus) { _ in
+            }
+            
+            .onChange(of: viewModel.binding.isSignInStatus) { _ in
+
                 presentationMode.wrappedValue.dismiss()
-            }
-            .fullScreenCover(isPresented: $viewModel.binding.isTermsOfServiceSheetFlag) {
-                TermsOfServiceView()
-            }
-            .fullScreenCover(isPresented: $viewModel.binding.isPrivacyPolicySheetFlag) {
-                PrivacyPolicyView()
+            
             }
         }
     }
@@ -125,10 +197,10 @@ extension LoginView {
             .italic()
     }
     
-    var signInStatusText: some View {
-        Text("\(viewModel.binding.isSignInStatusMessege)")
+    var signInErrorText: some View {
+        Text("\(viewModel.output.isSignInErrorMessege)")
             .font(.system(size: 16, weight: .light, design: .default))
-            .foregroundColor(viewModel.binding.isSignInStatusMessegeColorFlag ? .white : .red)
+            .foregroundColor(.red)
     }
 
     var emailHintText: some View {
@@ -144,27 +216,21 @@ extension LoginView {
     }
     
     var emailTextField: some View {
-        VStack {
             TextField("メールアドレス", text: $viewModel.binding.isEntryEmailTextField)
                 .preferredColorScheme(.light)
                 .autocapitalization(.none)
-        }
     }
     
     var passwordHideTextField: some View {
-        VStack {
             SecureField("8〜16文字のパスワード", text: $viewModel.binding.isEntryPasswordTextField)
                 .preferredColorScheme(.light)
                 .autocapitalization(.none)
-        }
     }
     
     var passwordOpenTextField: some View {
-        VStack {
             TextField("8〜16文字のパスワード", text: $viewModel.binding.isEntryPasswordTextField)
                 .preferredColorScheme(.light)
                 .autocapitalization(.none)
-        }
     }
     
     var passwordShowActionText: some View {
@@ -183,77 +249,120 @@ extension LoginView {
             }, label: {
                 Text("開始する")
                     .frame(width: 200, height: 40, alignment: .center)
-                    .font(.system(size: 16, weight: .regular, design: .default))
-                    .background(Color("Primary").opacity(0.3))
+                    .font(.system(size: 16, weight: .black, design: .default))
+                    .background(Color("Primary").opacity(viewModel.binding.isEntryEmailTextField.isEmpty || viewModel.binding.isEntryPasswordTextField.isEmpty ? 0.3 : 0.9))
                     .foregroundColor(.white)
-                    .cornerRadius(9)
+                    .cornerRadius(6)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(ColorComponents.gray200, lineWidth: 0.5)
+                    )
             }
         )
         .frame(width: 200, height: 40, alignment: .center)
+        .disabled(viewModel.binding.isEntryEmailTextField.isEmpty || viewModel.binding.isEntryPasswordTextField.isEmpty)
     }
     
-    var termsOfServiceText: some View {
+    var termsOfServiceButton: some View {
         Button(
+            
             action: {
+            
                 viewModel.input.toTermsOfServiceButtonTapped.send()
+                
             }, label: {
+                
                 Text("利用規約")
                     .foregroundColor(.white)
                     .underline()
+                
             }
-        )
+            
+        ).fullScreenCover(isPresented: $viewModel.binding.isTermsOfServiceSheetFlag) {
+            
+            TermsOfServiceView()
+            
+        }
     }
     
-    var privacyPolicyText: some View {
+    var privacyPolicyButton: some View {
         Button(
+            
             action: {
+                
                 viewModel.input.toPrivacyPolicyButtonTapped.send()
+            
             }, label: {
+            
                 Text("プライバシーポリシー")
                     .foregroundColor(.white)
                     .underline()
+            
             }
-        )
+        
+        ).fullScreenCover(isPresented: $viewModel.binding.isPrivacyPolicySheetFlag) {
+            
+            PrivacyPolicyView()
+        
+        }
     }
     
     var accountLoginText: some View {
-        HStack {
+        HStack(alignment: .center, spacing: 15) {
+            
             Rectangle()
-                .frame(height: 1)
-                .foregroundColor(Color("Gray100"))
+                .frame(height: 0.5)
+                .foregroundColor(ColorComponents.gray100)
                 .cornerRadius(24)
-            Spacer()
+            
             Text("サービスで開始する")
                 .font(.system(size: 16, weight: .light, design: .default))
-                .foregroundColor(Color("Gray100"))
+                .foregroundColor(ColorComponents.gray100)
                 .lineLimit(1)
                 .frame(width: 140)
-            Spacer()
+            
             Rectangle()
-                .frame(height: 1)
-                .foregroundColor(Color("Gray100"))
+                .frame(height: 0.5)
+                .foregroundColor(ColorComponents.gray100)
                 .cornerRadius(24)
-        }.padding(.horizontal, 20)
+            
+        }
+    }
+    
+    var signInWithAnonymouslyButton: some View {
+        Button(
+            action: {
+                
+                viewModel.input.toSignInWithAnonymouslyButtonTapped.send()
+            
+            }, label: {
+            
+                Text("ゲストで開始する")
+                    .frame(width: 200, height: 40, alignment: .center)
+                    .font(.system(size: 16, weight: .black, design: .default))
+                    .background(ColorComponents.primary)
+                    .foregroundColor(.white)
+                    .cornerRadius(6)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(ColorComponents.gray200, lineWidth: 0.5)
+                    )
+
+            }
+        ).frame(width: 200, height: 40, alignment: .center)
     }
     
     var signInWithGoogleButton: some View {
-        HStack {
-            Spacer()
             SignInWithGoogleButton(signInFlag: $viewModel.binding.isSignInStatus)
                 .frame(width: 206, height: 50, alignment: .center)
-            Spacer()
-        }
     }
     
     var signInWithAppleButton: some View {
-        HStack {
-            Spacer()
             SignInWithAppleButton(signInFlag: $viewModel.binding.isSignInStatus)
                 .frame(width: 200, height: 40, alignment: .center)
-            Spacer()
-        }
     }
 }
+
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView(viewModel: MockViewModel())
@@ -270,8 +379,11 @@ extension LoginView_Previews {
             var toTermsOfServiceButtonTapped: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>()
             var toPrivacyPolicyButtonTapped: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>()
             
+            var toSignInWithAnonymouslyButtonTapped: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>()
             var toSignInWithAppleButtonTapped: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>()
             var toSignInWithGoogleButtonTapped: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>()
+            
+            var toReWrithButtonTapped: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>()
         }
         
         final class Binding: LoginViewModelBindingObject {
@@ -283,11 +395,13 @@ extension LoginView_Previews {
             @Published var isEntryPasswordShowFlag: Bool = false
             
             @Published var isSignInStatus: Bool = false
-            @Published var isSignInStatusMessege: String = ""
-            @Published var isSignInStatusMessegeColorFlag: Bool = false
+            
+            @Published var isScrollOffsetFlag: Bool = false
         }
         
         final class Output: LoginViewModelOutputObject {
+            @Published var isSignInErrorMessege: String = ""
+            @Published var isSignInResultMessege: String = ""
         }
         
         var input: Input
